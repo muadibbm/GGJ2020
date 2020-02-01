7200cdcc0320abca2c965260cc1db43240952813
muadibbm@yahoo.com <muadibbm@yahoo.com>
2020-01-31 21:25:46 -0500

    Implemented managing components

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    public bool MouseButtonLeft { get; private set; }
+    public bool MouseButtonRight { get; private set; }
+    public Vector3 MousePosition { get; private set; }
+
+    private void Update()
+    {
+        MouseButtonLeft = Input.GetMouseButtonDown(0);
+        MouseButtonRight = Input.GetMouseButtonDown(1);
+        MousePosition = Input.mousePosition;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    private Dictionary<string, Component> tools = new Dictionary<string, Component>();
+
+    public static GameManager Instance {
+        get { return GetInstance(); }
+        set { _instance = value; }
+    }
+
+    private static GameManager _instance;
+
+    private static GameManager GetInstance() {
+        if (_instance == null) {
+            var go = new GameObject("Toolbox");
+            DontDestroyOnLoad(go);
+            _instance = go.AddComponent<GameManager>();
+        }
+        return _instance;
+    }
+
+    private void Awake() {
+        this.AddTool<GameInput>("GameInput");
+    }
+
+    public ObjType GetTool<ObjType>(string objName) where ObjType : Component {
+        return tools[objName] as ObjType;
+    }
+
+    public ObjType AddTool<ObjType>(string objName) where ObjType : Component {
+        var tool = new GameObject(objName);
+        tool.transform.SetParent(this.transform);
+        ObjType obj = tool.AddComponent<ObjType>();
+        this.tools.Add(objName, obj);
+        return obj;
+    }
+}
